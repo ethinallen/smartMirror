11b13cb21fd1c74551f502c8e5946d2d5810ec1d
Drew Emery <abemery@clemson.edu>
2019-06-15 19:16:12 -0400

    updateForecasts will create a new png in the directory

--- main.py
+++ main.py
@@ -1,27 +1,64 @@
 import requests
 import json
+import matplotlib.pyplot as plt
+import time
+import os
+
 
 class mirror():
 
     def __init__(self):
         with open('api.json') as data:
-            data = json.load(data)
-            key = data['key']
-            coord = data['coord']
-        self.weatherURL = 'https://api.darksky.net/forecast/' + key + '/' + coord
+            creds = json.load(data)
+            key = creds['key']
+            coord = creds['coord']
+            self.weatherURL = 'https://api.darksky.net/forecast/' + key + '/' + coord
+            self.times = []
+            self.temps = []
+            self.precip = []
+            self.xlabels = []
+            self.sun = None
 
     # update the weather
-    def updateWeather(self):
+    def updateForecasts(self):
+        files = [f for f in os.listdir('.') if os.path.isfile(f)]
+        for f in files:
+            if f == 'weiners.png':
+                os.remove(f)
+                print('REMOVED FILE')
         r = requests.get(self.weatherURL)
         data = r.json()
-        todaySummary = data['currently']['summary']
-        dailySummary = data['daily']['summary']
-        return dailySummary, todaySummary
-        print('TODAY:   {}'.format(todaySummary))
-        print('DAILY:   {}'.format(dailySummary))
+        # for key in data['daily']:
+        #     print(key)
+        data = data['hourly']['data']
 
 
+        i = 0
+        j = 0
+
+        # only saves values within a certain time frames
+        for index, element in enumerate(data):
+            if index < .24 * (len(data)):
+                if j == 5:
+                    self.xlabels.append(time.strftime('%m-%d %H:%M', time.localtime(element['time'])))
+                self.times.append(time.strftime('%m-%d %H:%M', time.localtime(element['time'])))
+                self.temps.append(element['temperature'])
+                self.precip.append(element['precipProbability'])
+            if j > 10:
+                j = 0
+
+        # all of the plot attributes
+        plt.style.use('dark_background')
+        plt.rcParams['axes.facecolor'] = 'black'
+        plt.rcParams['savefig.facecolor'] = 'black'
+
+        # Plot the data and set the labels.
+        plt.xticks(color='black')
+        plt.bar(self.times, self.temps, color='w')
+        plt.ylabel('TEMP')
+        plt.savefig('weiners.png', dpi = 1800)
 
 if __name__ == '__main__':
+    # print('hello')
     m = mirror()
-    m.updateWeather()
+    m.updateForecasts()
