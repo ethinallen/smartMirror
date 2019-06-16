ecbecc319c40dfc692a0339cd9230af367c0d35b
Drew Emery <abemery@clemson.edu>
2019-06-15 22:43:54 -0400

    push for alex

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import json
 import matplotlib.pyplot as plt
 import time
 import os
-import numpy as np
 import twilio
 
 class mirror():
@@ -24,7 +23,7 @@ class mirror():
     def updateForecasts(self):
         files = [f for f in os.listdir('.') if os.path.isfile(f)]
         for f in files:
-            if f == 'TEMPERATURE.png' or f == 'PRECIPITATION.png':
+            if f == 'weiners.png':
                 os.remove(f)
         r = requests.get(self.weatherURL)
         data = r.json()
@@ -46,24 +45,18 @@ class mirror():
                 self.precip.append(element['precipProbability'])
             if j > 10:
                 j = 0
-        for element in [['TEMPERATURE', self.temps], ['PRECIPITATION', self.precip]]:
 
+        # all of the plot attributes
+        plt.style.use('dark_background')
+        plt.rcParams['axes.facecolor'] = 'black'
+        plt.rcParams['savefig.facecolor'] = 'black'
 
-            if (element[0] == 'PRECIPITATION' and np.mean(element[1]) > .05) or element[0] == 'TEMPERATURE':
-                # all of the plot attributes
-                plt.style.use('dark_background')
-                plt.rcParams['axes.facecolor'] = 'black'
-                plt.rcParams['savefig.facecolor'] = 'black'
-
-                # Plot the data and set the labels.
-                plt.xticks(color='black')
-                plt.bar(self.times, element[1], color='w')
-                plt.ylabel(ylabel = '', size=30)
-                plt.tick_params(axis='both', which='major', labelsize=20)
-                plt.savefig((element[0] + '.png'), dpi = 1800)
-                plt.close()
+        # Plot the data and set the labels.
+        plt.xticks(color='black')
+        plt.bar(self.times, self.temps, color='w')
+        plt.ylabel('TEMP')
+        plt.savefig('weiners.png', dpi = 1800)
 
 if __name__ == '__main__':
-    # print('hello')
     m = mirror()
     m.updateForecasts()
